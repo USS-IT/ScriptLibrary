97c7b2b635d8eaeec093a29b2f70b185c1e222cc
Matt Carras <mattc.github@jhu.edu>
2025-04-15 15:30:28 -0400

    Update UninstallScriptPS.ps1 to v1.2
    - Added support for QuietUninstallString
    - Added support for passing exit codes
    - Script will now give a non-zero exit code if the install key still exists after the uninstall attempt.

--- SCCM/Packages/Generic Uninstaller/UninstallScriptPS.ps1
+++ SCCM/Packages/Generic Uninstaller/UninstallScriptPS.ps1
@@ -3,7 +3,7 @@
 	Attempts to silently uninstall software given its name.
 	
 	.DESCRIPTION
-	Attempts to silently uninstall software given its name from the registry. Usually this will be the same name as Add/Remove Programs.
+	Attempts to silently uninstall software given its name from the registry. Usually this will be the same name as Add/Remove Programs. The script will throw an error if the uninstall keys still exists after an uninstallation attempt is done.
     
 	.PARAMETER SoftwareName
 	The display name of the software to uninstall (Required). This should be the same as the one from Add/Remove Programs. Can be a partial, in which case it will attempt to uninstall the first match found. 
@@ -49,7 +49,12 @@
 	Make sure to test the uninstall outside of SCCM in case there's an uncaught prompt. If you still see a prompt, call the script using -OutputOnly to see what the parameters are. Then try opening up an admin command prompt and use common parameters like "setup.exe -?" or "setup.exe /?" to see if it displays a list of parameters (where setup.exe is the uninstaller filename). If that doesn't work, check online for any documentation on the uninstaller.
 
 	Author: mcarras8
-	Version: 1.1
+	Version: 1.2
+	
+	Changelog
+	04-15-2025 - mcarras8 - Added support for QuietUninstallString. 
+						  - Added support for passing exit codes.
+						  - Script will now give a non-zero exit code if the install key still exists after the uninstall attempt.
 #>
 
 param(
@@ -83,18 +88,30 @@ $_scriptName = split-path $PSCommandPath -Leaf
 # Check 32-bit, then 64-bit registry nodes.
 $installLocation = $null
 $uninstallString = $null
+$hasQuietUninstallParams = $false
 $installKey = gci "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { (($ExactMatch -And $_.DisplayName -eq $SoftwareName -And ([string]::IsNullOrEmpty($Publisher) -Or $_.Publisher -eq $Publisher)) -Or (-Not $ExactMatch -And $_ -match $SoftwareName -And ([string]::IsNullOrEmpty($Publisher) -Or $_.Publisher -match $Publisher))) -And ([string]::IsNullOrEmpty($Version) -Or $_.DisplayVersion -eq $Version) }
 $installLocation = $installKey | Select -ExpandProperty InstallLocation
-$uninstallString = $installKey | Select -ExpandProperty UninstallString
+if (-Not [string]::IsNullOrWhitespace($installKey.QuietUninstallString)) {
+	$uninstallString = $installKey | Select -ExpandProperty QuietUninstallString
+	$hasQuietUninstallParams = $true
+} else {
+	$uninstallString = $installKey | Select -ExpandProperty UninstallString
+}
 if ([string]::IsNullOrWhitespace($uninstallString)) {
 	$installKey = gci "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { (($ExactMatch -And $_.DisplayName -eq $SoftwareName -And ([string]::IsNullOrEmpty($Publisher) -Or $_.Publisher -eq $Publisher)) -Or (-Not $ExactMatch -And $_ -match $SoftwareName -And ([string]::IsNullOrEmpty($Publisher) -Or $_.Publisher -match $Publisher))) -And ([string]::IsNullOrEmpty($Version) -Or $_.DisplayVersion -eq $Version) }
 	$installLocation = $installKey | Select -ExpandProperty InstallLocation
-	$uninstallString = $installKey | Select -ExpandProperty UninstallString
-	if ([string]::IsNullOrWhitespace($uninstallString)) {
-		throw "[$_scriptName] Unable to find uninstall string for [$SoftwareName] in Registry, aborting"
+	if (-Not [string]::IsNullOrWhitespace($installKey.QuietUninstallString)) {
+		$uninstallString = $installKey | Select -ExpandProperty QuietUninstallString
+		$hasQuietUninstallParams = $true
+	} else {
+		$uninstallString = $installKey | Select -ExpandProperty UninstallString
 	}
 }
 
+if ([string]::IsNullOrWhitespace($uninstallString)) {
+	Write-Warning "[$_scriptName] Unable to find uninstall string for [$SoftwareName] in Registry, aborting"
+	exit 0
+}
 Write-Host "[$_scriptname] DisplayName = [$($installKey.DisplayName)]"
 Write-Host "[$_scriptname] Publisher = [$($installKey.Publisher)]"
 Write-Host "[$_scriptname] DisplayVersion = [$($installKey.DisplayVersion)]"
@@ -105,7 +122,8 @@ If (-Not $OutputOnly) {
 	$msiGUID = $null
 	if ( $uninstallString -match '^MsiExec(?:\.exe) /[XIxi]\s?({[^}]+})') {
 		if($ISSRecordResponseFile) {
-			throw "[$_scriptName] Parsed MsiExec setup is incompatible with -ISSRecordResponseFile"
+			Write-Error "[$_scriptName] Parsed MsiExec setup is incompatible with -ISSRecordResponseFile"
+			exit 1
 		}
 		$uninstallExe = 'Msiexec.exe'
 		$msiGUID = $Matches[1]
@@ -118,21 +136,22 @@ If (-Not $OutputOnly) {
 			$Params = $Matches[2] -split " " | where {-not [string]::IsNullorWhitespace($_)}
 			if($ISSRecordResponseFile) {
 				$Params += @("-uninst","-r","-f1""$ISSRecordResponseFile""")
-			} else {
+			} elseif (-Not $hasQuietUninstallParams) {
 				$Params += @("-uninst","-s")
 			}
 		} else {
 			# All other parameters.
 			if ( $uninstallString -match '^"?([^"]+\.(?cmd|bat))"?(.+)') {
 				if($ISSRecordResponseFile) {
-					throw "[$_scriptName] Parsed Cmd/Bat setup is incompatible with -ISSRecordResponseFile"
+					Write-Error "[$_scriptName] Parsed Cmd/Bat setup is incompatible with -ISSRecordResponseFile"
+					exit 2
 				}
 				$uninstallExe = $Matches[1]
 				$Params = $Matches[2] -split " " | where {-not [string]::IsNullorWhitespace($_)}
 			}
 		}
 		# Additional parameters given to the setup utility.
-		If ($Parameters) {
+		If (-Not [string]::IsNullOrEmpty($Parameters | Select -First 1)) {
 			If ($OverrideParameters) {
 				If (-Not $msiGUID) {
 					$Params = $Parameters
@@ -153,52 +172,51 @@ If (-Not $OutputOnly) {
 	}
 
 	if ([string]::IsNullOrWhitespace($uninstallExe)) {
-		throw "[$_scriptName] Unable to parse uninstall string for [$softwareName], aborting"
+		Write-Error "[$_scriptName] Unable to parse uninstall string for [$softwareName], aborting"
+		exit 3
+	}
+	
+	# & $uninstallExe $Params
+	# Start uninstaller and wait for it to finish before continuing.
+	$procParams = @{}
+	If (-Not [string]::IsNullOrEmpty($Params | Select -First 1)) {
+		Write-Host("[$_scriptname] Calling: $uninstallExe " + ($Params -join " "))
+		$procParams.Add("ArgumentList", $Params)
 	} else {
-		<#
-		# Add the setup ISS file for InstallShield setup, if needed.
-		if (-not [String]::IsNullOrEmpty($UninstallISS)) {
-			$Params += @("-uninst")
-			$Params += @("-s")
-			if($UninstallISS -notlike '*\*') { 
-				$Params += @("-f1""$PSScriptRoot\$UninstallISS""")
-			} else {
-				$Params += @("-f1""$UninstallISS""")
+		Write-Host("[$_scriptname] Calling: $uninstallExe")
+	}
+	$process = Start-Process -FilePath $uninstallExe @procParams -NoNewWindow -PassThru
+	$handle = $process.Handle # Cache the process handle
+	If ($Timeout) {
+		$process | Wait-Process -Timeout $Timeout
+	} else {
+		$process | Wait-Process
+	}
+	$exitCode = $process.ExitCode
+	
+	# If option given, wait until the InstallLocation is gone before continuing.
+	If($WaitInstallLocationDeleted -And -not [string]::IsNullOrWhitespace($installLocation)) {
+		if ((Test-Path $installLocation -PathType Container)) {
+			$msg = "[$_scriptName] InstallLocation [$installLocation] still found after uninstallation"
+			if ($WaitInstallLocationDeletedTimeout -gt 0) {
+				$msg += ", waiting up to $WaitInstallLocationDeletedTimeout seconds..."
 			}
+			Write-Host $msg
 		}
-		#>
-		# & $uninstallExe $Params
-		# Start uninstaller and wait for it to finish before continuing.
-		$procParams = @{}
-		If ($Params) {
-			Write-Host("[$_scriptname] Calling: $uninstallExe {0}" -f ($Params -join " "))
-			$procParams.Add("ArgumentList", $Params)
-		} else {
-			Write-Host("[$_scriptname] Calling: $uninstallExe")
-		}
-		$process = Start-Process -FilePath $uninstallExe @procParams -NoNewWindow -PassThru
-		If ($Timeout) {
-			$process | Wait-Process -Timeout $Timeout
-		} else {
-			$process | Wait-Process
-		}
-		# If option given, wait until the InstallLocation is gone before continuing.
-		If($WaitInstallLocationDeleted -And -not [string]::IsNullOrWhitespace($installLocation)) {
-			if ((Test-Path $installLocation -PathType Container)) {
-				$msg = "[$_scriptName] InstallLocation [$installLocation] still found after uninstallation"
-				if ($WaitInstallLocationDeletedTimeout -ne $null) {
-					$msg += ", waiting up to $WaitInstallLocationDeletedTimeout seconds..."
-				}
-				Write-Host $msg
-			}
-			$sleepTime = 10
-			$counter = 0
-			if ($WaitInstallLocationDeletedTimeout -ne $null) {
-				while($counter -le $WaitInstallLocationDeletedTimeout -And (Test-Path $installLocation -PathType Container)) {
-					Start-Sleep $sleepTime
-					$counter += $sleepTime
-				}
-			}
+		$sleepTime = 10
+		$counter = 0
+		while($counter -le $WaitInstallLocationDeletedTimeout -And (Test-Path $installLocation -PathType Container)) {
+			Start-Sleep $sleepTime
+			$counter += $sleepTime
 		}
 	}
+	
+	# Check to see if the registry key is gone.
+	if (Test-Path $installKey.PsPath) {
+		Write-Error "Install key still exists after uninstallation attempt: $($installKey.PSPath)"
+		exit 4
+	}
+	
+	# Return process exit code
+	exit $exitCode
 }
