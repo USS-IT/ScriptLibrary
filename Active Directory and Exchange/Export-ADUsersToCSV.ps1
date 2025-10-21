6bda5d4d76c6c566297ba4af32e60f0072bda794
Matt Carras <mattc.github@jhu.edu>
2025-10-21 11:43:53 -0400

    Update Export-ADUsersToCSV.ps1
    Fixed mixed groups that don't contain only users, fixed Get-ADGroup fallback

--- Active Directory and Exchange/Export-ADUsersToCSV.ps1
+++ Active Directory and Exchange/Export-ADUsersToCSV.ps1
@@ -7,16 +7,24 @@
 	
     .NOTES
 	Created: 6-8-23
-    Author: mcarras8
+    	Author: mcarras8
 #>
 $groupname = Read-Host "Enter AD group name"
 $outputFile = Read-Host "Output to CSV file (leave blank to show in pop-up)"
 try {
-	$results = Get-ADGroupMember -Recursive $groupname | foreach { Get-ADUser $_ -Properties Department,Company,DisplayName,mail,distinguishedname,extensionattribute2 } | Select Name,mail,DisplayName,Department,Company,extensionattribute2,distinguishedname
+	$results = Get-ADGroupMember -Recursive $groupname
+	$userResults = $results | where {$_.objectClass -eq "user"} | foreach { Get-ADUser $_ -Properties Department,Company,DisplayName,mail,distinguishedname,extensionattribute2 } | Select Name,mail,DisplayName,Department,Company,extensionattribute2,distinguishedname
+	if ($userResults -ne $null -And $userResults -isnot [array]) {
+		$userResults = @($userResults)
+	}
+	$otherResults = $results | where {$_.objectClass -ne "user"} | Select Name,@{N="extensionattribute2"; Expression={$_.objectClass}},distinguishedname
+	if ($otherResults -ne $null -And $otherResults -isnot [array]) {
+		$otherResults = @($otherResults)
+	}
+	$results = $userResults + $otherResults
 } catch {
-	$results = Get-ADGroup $groupname | Select -Expandproperty member | foreach { Get-ADUser $_ -Properties Department,Company,DisplayName,mail,distinguishedname,extensionattribute2 }
+	$results = Get-ADGroup $groupname -Properties member | Select -Expandproperty member | foreach { Get-ADUser $_ -Properties Department,Company,DisplayName,mail,distinguishedname,extensionattribute2 } | Select Name,mail,DisplayName,Department,Company,extensionattribute2,distinguishedname
 }
-$results = $results | Select Name,mail,DisplayName,Department,Company,extensionattribute2,distinguishedname
 
 if (-Not [string]::IsNullOrEmpty($outputFile)) {
 	if($outputFile -notlike "*\*") {
