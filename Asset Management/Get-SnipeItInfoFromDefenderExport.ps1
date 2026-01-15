162a71e3715e7653c17e6191cc362ff8c0d795b0
mattcarras <mattc.github@jhu.edu>
2026-01-15 12:29:14 -0500

    Update Get-SnipeItInfoFromDefenderExport.ps1

--- Asset Management/Get-SnipeItInfoFromDefenderExport.ps1
+++ Asset Management/Get-SnipeItInfoFromDefenderExport.ps1
@@ -5,47 +5,73 @@
 	.DESCRIPTION
 	Cross-references an export from the Defender Health Report with the latest Snipe-It export.
 
+	.PARAMETER CSV
+	The Defender export as a CSV file.
+	
+	.PARAMETER ExportType
+	Optional. Determines the primary matching column and which columns was exported from source. Only option currently is "MissingPatch".
+	
 	.NOTES	
 	Created: 5-20-25
-	Author: mcarras8
+	Author: Matt Carras (mcarras8)
 #>
-$defcomps = Import-CSV ".\Hidden-Defender Missing Patch Month.csv" | where {-Not [string]::IsNullOrEmpty($_.Computer)}
-$spcomps = Import-CSV "\\win.ad.jhu.edu\cloud\hsa$\ITServices\Reports\SnipeIt\Exports\assets_snipeit_latest.csv" | where {$_.name -in $defcomps.Computer}
+param(
+	[DefaultParameterSetName="Default"]
+	
+	[Parameter(Position=0,Mandatory=$true)]
+	[string] $CSV,
+	
+	[Parameter(ParameterSetName="Default")]
+	[ValidateSet("MissingPatch")]
+	[string] $ExportType="MissingPatch"
+)
+
+# -- START CONFIGURATION --
+# Path to latest Snipe-It export CSV
+$SNIPEIT_EXPORT_CSV = "\\win.ad.jhu.edu\cloud\hsa$\ITServices\Reports\SnipeIt\Exports\assets_snipeit_latest.csv"
+# Root of the Snipe-it instance for links
+$SNIPEIT_ROOT_URL = "https://jh-uss.snipe-it.io"
+# -- END CONFIGURATION
+
+$defcomps = Import-CSV $CSV | where {-Not [string]::IsNullOrEmpty($_.Computer)}
+$spcomps = Import-CSV $SNIPEIT_EXPORT_CSV
 $comps = $defcomps | % { 
 	$name = $_.Computer;
 	if(-Not [string]::IsNullOrWhitespace($Name)) { 
 		$comp = $spcomps | where {$_.name -eq $name} | Select -First 1; 
 		if (-Not [string]::IsNullOrEmpty($comp.name)) {
-			[PSCustomObject]@{
+			[PSCustomObject][ordered]@{
 				name = $name
 				Earliest_Missing_Patch = $_.Earliest_Missing_Patch
 				Months = $_.Months
 				Month_Group = $_.Month_Group
 				asset_tag = $comp.asset_tag
+				Status = $comp.status_label
 				Department = $comp.Department
 				assigned_to = $comp.assigned_to
 				"Primary Users" = $comp."Primary Users"
 				"AD LastLogonTime" = $comp."AD LastLogonTime"
 				"SCCM LastActiveTime" = $comp."SCCM LastActiveTime"
+				"OS Version" = $comp."OS Version"
 				created_at = $comp.created_at
-				Status = $comp.status_label
 				model = $comp.model
-				URL = "https://jh-uss.snipe-it.io/hardware/bytag?assetTag=$($comp.asset_tag)"
+				URL = "$SNIPEIT_ROOT_URL/hardware/bytag?assetTag=$($comp.asset_tag)"
 			}
 		} else {
-			[PSCustomObject]@{
+			[PSCustomObject][ordered]@{
 				name = $name
 				Earliest_Missing_Patch = $_.Earliest_Missing_Patch
 				Months = $_.Months
 				Month_Group = $_.Month_Group
-				asset_tag = ""
+				asset_tag = "<NOT FOUND>"
+				Status = ""
 				Department = ""
 				assigned_to = ""
 				"Primary Users" = ""
 				"AD LastLogonTime" = ""
 				"SCCM LastActiveTime" = ""
+				"OS Version" = ""
 				created_at = ""
-				Status = ""
 				model = ""
 				URL = ""
 			}
