a314ddd407d0b9ad8416e2ed014ca5e4185b0ab0
Matt Carras <mattc.github@jhu.edu>
2026-06-26 16:34:43 -0400

    Initial upload for new SchTask VPN scripts
    Allows switching between VPN profiles without admin

---
+++ SCCM/Package Scripts/New-SwitchVPNProfile/VPNGeneral.cmd
@@ -0,0 +1,9 @@
+@echo off
+REM Switch to Ivanti VPN General as standard user
+REM Requires installing:
+REM - VPN Utilities
+REM - Install-Tasks-SwitchVPNProfile
+REM Author: Matt Carras (mcarras8)
+REM Created: 6-26-2026
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Write-EventLog -LogName 'USS-EventLog' -Source 'SwitchVPNProfile-General' -EventID 1000 -EntryType Information -Message 'Trigger VPN switch to General'"
