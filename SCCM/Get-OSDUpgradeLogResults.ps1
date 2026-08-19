9010a6fc05b28ad1f90f91bc38e7581f1f34a21f
Matt Carras <mattc.github@jhu.edu>
2026-08-19 10:21:40 -0400

    Update Get-OSDUpgradeLogResults.ps1
    - Changed OutCSV param to CSV
    - Additional error checking
    - Fixed folder name parsing
    - Added additional columns to output

--- SCCM/Get-OSDUpgradeLogResults.ps1
+++ SCCM/Get-OSDUpgradeLogResults.ps1
@@ -4,12 +4,19 @@ param(
     [string] $LogsDir = '\\win.ad.jhu.edu\Data\osdlogs$\USS-ECI Upgrade Win11 25H2 x64 2026.02-V1.1',
 	
 	[Parameter(Mandatory=$true)]
-	[string] $OutCSV
+	[string] $CSV
 )
+
+if (!(Test-Path $LogsDir)) {
+	Write-Error "[$LogsDir] does not exist or is not accessible."
+	exit 1
+}
 
 Write-Host "Searching [$LogsDir] for result directories..."
 $results = Get-ChildItem -Path (Join-Path $LogsDir '\*') -Directory |
     ForEach-Object {
+		
+
 		try {
 			$foldername = $_.FullName -split '\\' | Select -Last 1
 			if (-Not $foldername) {
@@ -19,16 +26,8 @@ $results = Get-ChildItem -Path (Join-Path $LogsDir '\*') -Directory |
 		} catch {
 			Write-Warning "Error parsing [$($_.FullName)]: $($_.Exception.Message)"
 		}
-	
-		try {
-			$foldername = $_.FullName -split '\\' | Select -Last 1
-			if (-Not $foldername) {
-				Write-Warning "Problem parsing [$($_.FullName)]"
-				continue
-			}
-		} catch {
-			Write-Warning "Error parsing [$($_.FullName)]: $($_.Exception.Message)"
-		}
+		
+		Write-Host "Found [$($_.FullName)] with folder name [$foldername]"
 		
 		if ($foldername -match "([^\.]+)\.([\d\.]+)@([\d\s\.]+)\.(.+)`$") {
 			$systemName = $Matches[1]
@@ -72,10 +71,10 @@ $results = Get-ChildItem -Path (Join-Path $LogsDir '\*') -Directory |
 				'Time' = $logTime
 				'Error' = $lastop_error
 				'Last Op before Error' = $lastop
-				'Upgrade Start Time' = $upgradeStartTime
-				'Upgrade End Time' = $upgradeEndTime
-				'Rollback Start Time' = $rollbackStartTime
-				'Rollback End Time' = $rollbackEndTime
+				'Upgrade Start Time' = $upgradeStart
+				'Upgrade End Time' = $upgradeEnd
+				'Rollback Start Time' = $rollbackStart
+				'Rollback End Time' = $rollbackEnd
 				'Log Path' = $_.FullName
 			}
 		} else {
@@ -83,8 +82,33 @@ $results = Get-ChildItem -Path (Join-Path $LogsDir '\*') -Directory |
 		}
     }
 
-if ($results) {
+if (-Not $results -Or $results.Count -eq 0) {
 	Write-Warning "No results to export."
 } else {
-	$results | Export-CSV -NoTypeInformation $OutCSV
+	Write-Host "* Exporting $($results.Count) results to [$CSV]..."
+	# Add whether any items later upgraded successfully and attempt #.
+	$sysResults = @{}
+	foreach ($system in $results.System) {
+		if (-Not $sysResults.ContainsKey($system)) {
+			$sysResults[$system] = $results | where {$_.System -eq $system} | Sort Date,Time
+		}
+	}
+	$results | Select 'System','Result','Date','Time',
+		@{N="Eventual Success"; E={ 
+			if ($_.Result -ne 'upgrade_success') {
+				($sysResults[$_.System] | where {$_.Result -eq 'upgrade_success'} | Measure).Count -gt 0
+			} else {
+				''
+			}
+		}},
+		@{N="Attempt"; E={ 
+			if ($sysResults[$_.System] -is [array]) {
+				[Array]::IndexOf($sysResults[$_.System], $_) + 1
+			} else {
+				1
+			}
+		}},
+		'Error','Last Op before Error','Upgrade Start Time','Upgrade End Time','Rollback Start Time','Rollback End Time','Log Path' |
+		Sort Date,Time |
+		Export-CSV -NoTypeInformation $CSV
 }
