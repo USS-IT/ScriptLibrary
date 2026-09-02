e5d1b5e8c35365871623323fb7788c7e450b280c
Matt Carras <mattc.github@jhu.edu>
2026-09-02 15:58:30 -0400

    Update Enable_WiFi.cmd

--- SCCM/Package Scripts/Local Scripts/Reenable_WiFi/User/Enable_WiFi.cmd
+++ SCCM/Package Scripts/Local Scripts/Reenable_WiFi/User/Enable_WiFi.cmd
@@ -1,8 +1,8 @@
 @echo off
 SET "_LOGFILE=C:\USS\Logs\User\Reenable_WiFi\%~nx0.log"
 (
+	REM Show toast notification	
+	powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0Show-Toast.ps1" -Text "Attempting to re-enable the WiFi adapter. This may take a minute or two." -Title "WiFi re-enable" -LauncherID "%~f0"
 	powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0Register-Tasks-NotifyReenableWiFi.ps1"
 	powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Write-EventLog -LogName 'USS-EventLog' -Source 'Enable-WiFi' -EventID 1000 -EntryType Information -Message 'Trigger Re-enable WiFi Adapters'"
-	REM Show toast notification
-	powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0Show-Toast.ps1" -Text "Attempting to re-enable the WiFi adapter. This may take a minute or two." -Title "WiFi re-enable" -LauncherID "%~f0"
 ) > "%_LOGFILE%" 2>&1
